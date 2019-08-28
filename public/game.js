be15b32dd39fbe102eb99a88e86bd1e12be9cfd2
seunghun <seunghun.chan@gmail.com>
2019-08-29 00:07:13 +0900

    Added images

--- public/game.js
+++ public/game.js
@@ -1,10 +1,8 @@
-window.onload = function () {
-  var config = {
-    width: 800,
-    height: 600,
-    backgroundColor: 0x000000,
-    scene: [Scene1, Scene2]
-  };
+var config = {
+  width: 256,
+  height: 272,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2]
+};
 
-  var game = new Phaser.Game(config);
-}
+var game = new Phaser.Game(config);
